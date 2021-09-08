f2e824081d37ea0d57042c3b967fde8acecf4e28
musicbokov <79108359+musicbokov@users.noreply.github.com>
2021-09-08 23:04:36 +0400

    Настройки для production

--- config/web.php
+++ config/web.php
@@ -17,9 +17,6 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'tAR79Ux58-k_rPxF_kWMWQFsCLtqnMNs',
         ],
-//        'cache' => [
-//            'class' => 'yii\caching\FileCache',
-//        ],
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
