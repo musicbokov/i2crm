9992be6cc698d9e39a49a11f250c37b72f3ad526
Alexandr Bokov <a.bokov@yakovlev.studio>
2022-04-02 01:38:21 +0400

    docker-compose.yml

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=i2crm-mysql-1;dbname=i2crm_db',
+    'dsn' => 'mysql:host=html_mysql_1;dbname=i2crm_db',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
